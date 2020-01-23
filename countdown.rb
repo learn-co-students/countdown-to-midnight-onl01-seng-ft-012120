5a82b6955385a829238963081f9f529122d7c9e1
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-23 17:18:40 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+def countdown(ten)
+  while ten > 0
+    puts "#{ten} SECOND(S)!"
+    ten = ten - 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(five)
+  five.downto(0) do |seconds|
+    sleep 1
+  end
 end
