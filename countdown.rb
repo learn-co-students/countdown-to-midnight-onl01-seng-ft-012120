2cc5a9d2e6d4159686db14e2466ae8557c0b8008
Ola Enol <eniolar10@gmail.com>
2020-01-30 22:48:45 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown (x)
+  x = 9
+  while x = 0
+  puts "HAPPY NEW YEAR!"
+  X -= 1
 end
+
+def countdown_with_sleep (y)
+  time = Time.now
+    x = 9
+    sleep 1.second while x = 0
+    puts "HAPPY NEW YEAR!"
+    X -= 1
+  end
